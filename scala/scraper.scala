36b98fb546745ba44e6a43980fce87e401e83f0a
mpaulweeks <mpaulweeks@gmail.com>
2016-07-13 23:46:39 -0400

    stateless

--- scala/scraper.scala
+++ scala/scraper.scala
@@ -2,6 +2,7 @@
 import java.io._
 
 import scala.io.Source
+import scala.collection.breakOut
 import scala.collection.mutable
 import scala.util.parsing.json.JSON
 
@@ -15,47 +16,27 @@ case class VideoItem(timestamp: String, id: String, title: String) {
     )
 }
 
-case class ChannelInfo(fileName: String, playlistId: String)
-
-class VideoLibrary(fileName: String) {
+case class YouTubeChannel(fileName: String, playlistId: String) {
     private val DATA_FILE_PATH = s"data/$fileName.json"
-    private val videoList: mutable.ListBuffer[VideoItem] = mutable.ListBuffer.empty[VideoItem]
-    private val videoIds: mutable.Set[String] = mutable.Set()
 
-    private def loadFile(): Unit = {
+    def loadFile(): Map[String, VideoItem] = {
         if (!(new File(DATA_FILE_PATH).exists)){
-            return
+            throw new Exception
         }
         val jsonString = Source.fromFile(DATA_FILE_PATH).getLines.mkString
-        val jsonMap:List[Any] = JSON.parseFull(jsonString).get.asInstanceOf[List[Any]]
-        jsonMap.foreach( item => {
-            val videoTuple:List[String] = item.asInstanceOf[List[String]]
+        val jsonMap: List[Any] = JSON.parseFull(jsonString).get.asInstanceOf[List[Any]]
+        jsonMap.map { item =>
+            val videoTuple: List[String] = item.asInstanceOf[List[String]]
             val videoItem = new VideoItem(videoTuple(0), videoTuple(1), videoTuple(2))
-            add(videoItem)
-        })
-    }
-    loadFile
-
-    def add(video: VideoItem): Boolean = {
-        // for debugging
-        // if (videoIds.size > 9) {
-        //     return false
-        // }
-
-        if (!(videoIds contains video.id)){
-            videoList += video
-            videoIds.add(video.id)
-            return true
-        } else {
-            return false
-        }
+            (videoItem.id, videoItem)
+        }(breakOut)
     }
 
-    def toFile(): Unit = {
+    def toFile(videoMap: Map[String, VideoItem]): Unit = {
         val file = new File(DATA_FILE_PATH)
         val bw = new BufferedWriter(new FileWriter(file))
         var first = true
-        val sortedVideos = videoList.sortBy(r => (r.timestamp, r.id)).reverse
+        val sortedVideos = videoMap.values.toSeq.sortBy(r => (r.timestamp, r.id)).reverse
         sortedVideos.foreach( videoItem => {
             var jsonLine = videoItem.json
             if (first){
@@ -71,18 +52,43 @@ class VideoLibrary(fileName: String) {
     }
 }
 
-case class VideoFetcher(apiKey: String, channelInfo: ChannelInfo) {
+case class VideoFetcher(apiKey: String) {
     private val BASE_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
-    private val library = new VideoLibrary(channelInfo.fileName)
 
-    def fetchVideos(nextPageToken: String = ""): Unit = {
-        println("fetching videos")
+    def fetchVideos(channel: YouTubeChannel): Unit = {
+        val existingVideos = channel.loadFile
+        val updatedVideos = updateVideos(channel, existingVideos)
+        channel.toFile(updatedVideos)
+    }
 
+    private def updateVideos(
+        channel: YouTubeChannel,
+        videoMap: Map[String, VideoItem],
+        nextPageToken: String = ""
+    ): Map[String, VideoItem] = {
+        val newVideoData = pullVideoData(channel, nextPageToken)
+        val (newPageToken, newVideos) = processVideoData(newVideoData)
+
+        val newVideoMap = videoMap ++ newVideos
+        val oldData = newVideoMap.size == videoMap.size
+
+        if (newPageToken.length == 0 || oldData){
+            newVideoMap
+        } else {
+            updateVideos(channel, newVideoMap, newPageToken)
+        }
+    }
+
+    private def pullVideoData(
+        channel: YouTubeChannel,
+        nextPageToken: String
+    ): String = {
+        println(s"fetching videos for ${channel.fileName}")
         var request: HttpRequest = (
             Http(BASE_URL)
             .param("part", "snippet")
             .param("maxResults", "50")
-            .param("playlistId", channelInfo.playlistId)
+            .param("playlistId", channel.playlistId)
             .param("key", apiKey)
         )
         if (nextPageToken.length > 0){
@@ -90,51 +96,41 @@ case class VideoFetcher(apiKey: String, channelInfo: ChannelInfo) {
         }
         val response: HttpResponse[String] = request.asString
         println(response.code)
-        processVideos(response.body)
+        response.body
     }
 
-    private def processVideos(jsonString: String): Unit = {
-        val jsonMap:Option[Any] = JSON.parseFull(jsonString)
-        val resObj:Map[String,Any] = jsonMap.get.asInstanceOf[Map[String, Any]]
+    private def processVideoData(jsonString: String): (String, Map[String, VideoItem]) = {
+        val jsonMap: Option[Any] = JSON.parseFull(jsonString)
+        val resObj: Map[String,Any] = jsonMap.get.asInstanceOf[Map[String, Any]]
         var nextPageToken = ""
         if (resObj.contains("nextPageToken")){
             nextPageToken = resObj.get("nextPageToken").get.asInstanceOf[String]
         }
-        val videoItems:List[Any] = resObj.get("items").get.asInstanceOf[List[Any]]
-        var allUnique = true
-        videoItems.foreach( item => {
-            val itemMap:Map[String,Any] = item.asInstanceOf[Map[String,Any]]
-            val snippet:Map[String,Any] = itemMap.get("snippet").get.asInstanceOf[Map[String,Any]]
-            val timestamp:String = snippet.get("publishedAt").get.asInstanceOf[String]
-            val title:String = snippet.get("title").get.asInstanceOf[String]
-            val resource:Map[String,String] = snippet.get("resourceId").get.asInstanceOf[Map[String,String]]
-            val videoId:String = resource.get("videoId").get
-            val newVideo = new VideoItem(timestamp, videoId, title)
-
-            println(newVideo)
-            allUnique &= library.add(newVideo)
-        })
-        if (!allUnique){
-            println("dupe found, terminating")
-        } else if (nextPageToken.length == 0){
-            println("hit end of video list, termination")
-        } else {
-            fetchVideos(nextPageToken)
-            return
-        }
-        library.toFile
+        val videoItems: List[Any] = resObj.get("items").get.asInstanceOf[List[Any]]
+        val newVideos: Map[String, VideoItem] = videoItems.map { item =>
+            val itemMap: Map[String,Any] = item.asInstanceOf[Map[String,Any]]
+            val snippet: Map[String,Any] = itemMap.get("snippet").get.asInstanceOf[Map[String,Any]]
+            val timestamp: String = snippet.get("publishedAt").get.asInstanceOf[String]
+            val title: String = snippet.get("title").get.asInstanceOf[String]
+            val resource: Map[String,String] = snippet.get("resourceId").get.asInstanceOf[Map[String,String]]
+            val videoId: String = resource.get("videoId").get
+            val videoItem = new VideoItem(timestamp, videoId, title)
+            (videoItem.id, videoItem)
+        }(breakOut)
+        (nextPageToken, newVideos)
     }
 }
 
-object Demo {
+object Scraper {
 
-    val ChannelYogaFlame = new ChannelInfo("YogaFlame24", "UU1UzB_b7NSxoRjhZZDicuqw")
-    val ChannelOlympicGaming = new ChannelInfo("TubeOlympicGaming", "UUg5TGonF8hxVU_YVVaOC_ZQ")
+    val ChannelYogaFlame = new YouTubeChannel("YogaFlame24", "UU1UzB_b7NSxoRjhZZDicuqw")
+    val ChannelOlympicGaming = new YouTubeChannel("TubeOlympicGaming", "UUg5TGonF8hxVU_YVVaOC_ZQ")
 
     def run() {
         val apiKey = Source.fromFile("keys/youtube").getLines.next
-        new VideoFetcher(apiKey, ChannelYogaFlame).fetchVideos()
-        new VideoFetcher(apiKey, ChannelOlympicGaming).fetchVideos()
+        val fetcher = new VideoFetcher(apiKey)
+        fetcher.fetchVideos(ChannelYogaFlame)
+        fetcher.fetchVideos(ChannelOlympicGaming)
     }
 
     def main(args: Array[String]) {
