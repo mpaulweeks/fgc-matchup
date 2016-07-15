56025a0ef6e5fcd75f8f404b7ef6ac0a393fcb5f
mpaulweeks <mpaulweeks@gmail.com>
2016-07-15 02:17:48 -0400

    experiment with levenstein

--- js/player.js
+++ js/player.js
@@ -5,7 +5,11 @@ var PlayerManager = (function(){
     var registry = null;
 
     self.getDisplayName = function(playerName){
-        return registry[playerKey(playerName)].displayName;
+        var reg = registry[playerKey(playerName)];
+        while (reg.levensteinMatch){
+            reg = registry[reg.levensteinMatch];
+        }
+        return reg.displayName;
     }
 
     function PlayerItem(name){
@@ -36,6 +40,7 @@ var PlayerManager = (function(){
         if (!(key in registry)){
             registry[key] = {
                 displayName: null,
+                levensteinMatch: null,
                 count: 0,
                 matches: {},
             }
@@ -48,28 +53,58 @@ var PlayerManager = (function(){
         matches[playerName] += 1;
     }
 
+    function calculateLevenstein(){
+        var regKeys = Object.keys(registry);
+
+        var levenReq = 1;
+        regKeys.forEach(function (key1){
+            var reg1 = registry[key1];
+            var levenKey = null;
+            var levenMin = 100;
+            regKeys.forEach(function (key2){
+                if (key1 != key2){
+                    var distance = key1.levenstein(key2);
+                    if (distance <= levenReq && distance < levenMin){
+                        levenKey = key2;
+                        levenMin = distance;
+                    }
+                }
+            });
+            if (levenKey){
+                if (registry[levenKey].count > reg1.count){
+                    reg1.levensteinKey = levenKey;
+                    console.log(levenKey, key1);
+                }
+            }
+        });
+    }
+
     self.organize = function(playerNames){
         registry = {};
         playerNames.forEach(function (playerName){
             register(playerName);
         });
-        for (var key in registry){
-            if (registry.hasOwnProperty(key)){
-                var reg = registry[key];
-                var maxName = null;
-                var maxCount = 0;
-                for (var playerName in reg.matches){
-                    // determine best names
-                    if (reg.matches.hasOwnProperty(playerName)){
-                        if (reg.matches[playerName] > maxCount){
-                            maxName = playerName;
-                            maxCount = reg.matches[playerName];
-                        }
+
+        var regKeys = Object.keys(registry);
+
+        regKeys.forEach(function (key){
+            var reg = registry[key];
+            var maxName = null;
+            var maxCount = 0;
+            for (var playerName in reg.matches){
+                // determine best names
+                if (reg.matches.hasOwnProperty(playerName)){
+                    if (reg.matches[playerName] > maxCount){
+                        maxName = playerName;
+                        maxCount = reg.matches[playerName];
                     }
                 }
-                reg.displayName = maxName;
             }
-        }
+            reg.displayName = maxName;
+        });
+
+        // disabled for now, takes too long :(
+        // calculateLevenstein();
     }
 
     return self;
