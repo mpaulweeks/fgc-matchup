b26c346d8443bedd37aec5d4fc5869e2a8d3d1e7
mpaulweeks <mpaulweeks@gmail.com>
2016-07-23 04:21:45 -0400

    merge player into model.js

--- js/model.js
+++ js/model.js
@@ -1,5 +1,4 @@
 
-
 var GameManager = (function(){
     var self = {};
     var lookup = {};
@@ -47,6 +46,32 @@ var CharacterManager = (function(){
             lookup[charItem.id] = charItem;
         }
         return self.get(charItem.id);
+    }
+
+    return self;
+})();
+
+var PlayerManager = (function(){
+    var self = {};
+    var lookup = {};
+
+    function PlayerItem(name){
+        var item = {};
+        item.name = name;
+        item.id = TOOL.fixValue(item.name);
+        return item;
+    }
+
+    self.get = function(playerId){
+        return lookup[playerId];
+    }
+
+    self.new = function(playerName){
+        var playerItem = PlayerItem(playerName);
+        if (!(playerItem.id in lookup)){
+            lookup[playerItem.id] = playerItem;
+        }
+        return self.get(playerItem.id);
     }
 
     return self;
