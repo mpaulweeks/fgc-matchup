c85aa206f430be078f97bb50e0db76255e9a77ec
mpaulweeks <mpaulweeks@gmail.com>
2016-07-23 03:32:58 -0400

    port levenshtein logic to scala

--- js/player.js
+++ js/player.js
@@ -3,16 +3,6 @@ var PlayerManager = (function(){
     var self = {};
     var lookup = {};
 
-    self.getDisplayName = function(gameId, playerName){
-        // defunct and unused, keeping around until ported to scala
-        var registry = regByGame[gameId];
-        var reg = registry[playerKey(playerName)];
-        while (reg.levensteinKey && reg.count < registry[reg.levensteinKey].count){
-            reg = registry[reg.levensteinKey];
-        }
-        return reg.displayName;
-    }
-
     function PlayerItem(name){
         var item = {};
         item.name = name;
@@ -30,34 +20,6 @@ var PlayerManager = (function(){
             lookup[playerItem.id] = playerItem;
         }
         return self.get(playerItem.id);
-    }
-
-    function calculateLevenstein(registry){
-        // defunct and unused, keeping around until ported to scala
-        var regKeys = Object.keys(registry);
-
-        var levenReq = 1;
-        regKeys.forEach(function (key1, index1){
-            var reg1 = registry[key1];
-            var levenKey = reg1.levensteinKey;
-            var levenMin = reg1.levensteinDistance;
-            regKeys.forEach(function (key2, index2){
-                if (index1 < index2){
-                    var distance = key1.levenstein(key2);
-                    if (distance <= levenReq && (levenMin == null || distance < levenMin)){
-                        levenKey = key2;
-                        levenMin = distance;
-                    }
-                }
-            });
-            if (levenKey != reg1.levensteinKey){
-                reg1.levensteinKey = levenKey;
-                reg1.levensteinDistance = levenMin;
-                registry[levenKey].levensteinKey = key1;
-                registry[levenKey].levensteinDistance = levenMin;
-                console.log(levenKey, key1);
-            }
-        });
     }
 
     return self;
--- scala/normalizer.scala
+++ scala/normalizer.scala
@@ -2,6 +2,7 @@
 package fgc.normalizer
 
 import fgc.model.VideoData
+import fgc.levenshtein.EditDistance.editDist
 
 import scala.collection.mutable
 
@@ -47,12 +48,36 @@ object PlayerNormalizer {
             lookup(key) = stat.register(rawName)
         }
 
+        private def leveshtein(stats: List[AliasStat]): Map[String, String] = {
+            // need to recursively collapse levenshtein matches
+            // cut execution time in half by caching distances
+            println("starting levenshtein")
+            stats.map { as =>
+                val editDistances = stats.filter { os =>
+                    os.key != as.key
+                }.map{ os =>
+                    (os, editDist(as.key, os.key))
+                }
+                val closestEdit = editDistances.reduceLeft { (x,y) =>
+                    if (x._2 < y._2) x else y
+                }
+                var trueAlias = as
+                if (closestEdit._2 <= 1) {
+                    println(s"found close match! $as & $closestEdit")
+                    if (closestEdit._1.count > as.count){
+                        println(s"found replacement! $closestEdit")
+                        trueAlias = closestEdit._1
+                    }
+                }
+                (as.key, trueAlias.displayName)
+            }.toMap
+        }
+
         def generateKeyLookup(): Map[String, String] = {
             val stats = lookup.values.map(_.determineDisplayName)
-            // todo leveshtein
-            stats.map{ stat => 
-                (stat.key, stat.displayName)
-            }.toMap
+            // needs work
+            // leveshtein(stats.toList)
+            stats.map { as => (as.key, as.displayName)}.toMap
         }
     }
 
