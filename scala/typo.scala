118c4e11d4fabe37cd673dd700145cca0d37ab4d
mpaulweeks <mpaulweeks@gmail.com>
2016-08-08 14:36:26 -0400

    add player typo

--- scala/typo.scala
+++ scala/typo.scala
@@ -9,6 +9,7 @@ import fgc.model.Transform
 
 class TypoFixer() {
     private val CHARACTER_FILE_PATH = "reference/character_typo.json"
+    private val PLAYER_FILE_PATH = "reference/player_typo.json"
 
     private def loadFile(filePath: String): Map[String, List[String]] = {
         if (!(new File(filePath).exists)){
@@ -21,13 +22,21 @@ class TypoFixer() {
         }.toMap
     }
 
+    private def genInverse(nameMap: Map[String, List[String]]): Map[String, String] = {
+        nameMap.map { case(fix, typos) =>
+            typos.map(typo => (Transform.toKey(typo), Transform.toKey(fix)))
+        }.flatten.toMap
+    }
+
     private val charNameMap: Map[String, List[String]] = loadFile(CHARACTER_FILE_PATH)
-    private val charInverse = charNameMap.map { case(fix, typos) =>
-        typos.map(typo => (Transform.toKey(typo), Transform.toKey(fix)))
-    }.flatten.toMap
+    private val charInverse = genInverse(charNameMap)
+
+    private val playerNameMap: Map[String, List[String]] = loadFile(PLAYER_FILE_PATH)
+    private val playerInverse = genInverse(playerNameMap)
 
     def fixPlayer(rawName: String): String = {
-        Transform.toKey(rawName)
+        val key = Transform.toKey(rawName)
+        playerInverse.getOrElse(key, key)
     }
 
     def fixCharacter(rawName: String): String = {
